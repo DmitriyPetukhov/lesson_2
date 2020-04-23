c445ad9aeebee1900e48cfbd66bda1c4fa9c5472
Dmitriy Petukhov <61263313+Dmitrmi@users.noreply.github.com>
2020-04-23 20:49:55 +0300

    Add files via upload

--- fib.rb
+++ fib.rb
@@ -1,5 +1,5 @@
 fib = [1, 2]
 while fib[-1] + fib[-2] < 100 do
-	fib << fib[-1] + fib[-2]
+  fib << fib[-1] + fib[-2]
 end
 puts fib
--- year.rb
+++ year.rb
@@ -4,19 +4,17 @@ print "Вводим месяц :"
 month = gets.to_i
 print "Вводим год :"
 year = gets.to_i
-months = {
-  1 => 31, 
-  2 => 28,
-  3 => 31,
-  4 => 30,
-  5 => 31, 
-  6 => 30, 
-  7 => 31, 
-  8 => 31, 
-  9 => 30, 
-  10 => 31, 
-  11 => 30, 
-  12 => 31
-}
-result = months.take(month - 1).sum(number){|m, d| d}
-puts "#{result}"
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_year = 0
+day_year += number
+for d in 0 .. month - 2
+  day_year += months[d]
+end
+if year % 4 == 0 &&   
+  day_year += 1
+elsif year % 4 == 0 && year % 100 == 0
+  day_year += 0
+elsif year % 4 == 0 && year % 100 == 0 && year % 400 == 0
+  day_year += 1
+end
+puts "#{day_year}"
